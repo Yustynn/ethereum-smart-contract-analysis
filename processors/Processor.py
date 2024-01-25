d3cc93e8de9412fedf8d1997fce35574a55ff216
Yustynn Panicker <yustynn@gmail.com>
2024-01-25 14:53:43 +0700

    [Processor] Fix ABC implementation

--- processors/Processor.py
+++ processors/Processor.py
@@ -1,22 +1,17 @@
-"""Processor base class
-• Assumption that the output of run is a monoid instance
-"""
-
-from abc import abstractstaticmethod
-from typing import Any, Optional
+from abc import ABC, abstractmethod
 from slither import Slither
 from Monoid import Monoid
 import logging
 
-class Processor:
-  @abstractstaticmethod
+class Processor(ABC):
+  @staticmethod
+  @abstractmethod
   def run(slither: Slither) -> Monoid:
-    # return value's class should define __add__
     pass
 
   @classmethod
   def mk_logger(cls, level: int = logging.INFO) -> logging.Logger:
     lg = logging.getLogger(cls.__name__)
-    lg.setLevel(logging.INFO)
+    lg.setLevel(level=level)
 
     return lg
