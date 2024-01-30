f2e9763d22f93095405c62119018540192837e32
Yustynn Panicker <yustynn@gmail.com>
2024-01-30 18:25:53 +0700

    [gh-repos] Make repo language checking async

--- data-scripts/github_repos/github_repos.py
+++ data-scripts/github_repos/github_repos.py
@@ -1,12 +1,14 @@
 # note: GitHub rate limit using authenticated API is 5k per hour.
 
-from github import Github, Auth
+from gidgethub.aiohttp import GitHubAPI
 from dotenv import load_dotenv
-from typing import Dict, TypeAlias
+from typing import Dict, Optional, TypeAlias
 import os
 import json
+import aiohttp
 from tqdm import tqdm
 import logging
+import asyncio
 
 RESULTS_FOLDER = './results'
 load_dotenv('../../.env')
@@ -21,26 +23,51 @@ def solidity_fraction(languages: Languages) -> float:
 
     return languages['Solidity'] / sum(languages.values())
 
-g = Github(auth=Auth.Token(os.environ['GITHUB_TOKEN']))
 org_name = 'crypto-org-chain'
 
-results = []
-lg.info(f'[{org_name}] Retrieving repos')
-repos = list(g.get_user(org_name).get_repos())
-
-pbar = tqdm(repos)
-for repo in pbar:
-    pbar.set_description(f'[{org_name}/{repo.name}] Found {len(results)} solidity repos')
-    languages = repo.get_languages()
+async def process_repo(gh: GitHubAPI, org_name: str, repo: dict) -> Optional[dict]:
+    languages = await gh.getitem(f'/repos/{org_name}/{repo["name"]}/languages')
     if 'Solidity' in languages:
-        results.append({
-            'name': repo.name,
-            'url': repo.clone_url,
+        return {
+            'name': repo['name'],
+            'url': repo['clone_url'],
             'solidity_fraction': solidity_fraction(languages),
             'languages': languages,
-        })
+        }
 
-with open(os.path.join(RESULTS_FOLDER, f'{org_name}.json'), 'w') as f:
-    json.dump({'org_name': org_name, 'results': results}, f)
+ORG_NAMES = [
+    'decentraland',
+]
 
-g.close()
+async def main():
+    pbar = tqdm(ORG_NAMES)
+    for org_name in ORG_NAMES:
+        pbar.set_description(org_name)
+        results_path = os.path.join(RESULTS_FOLDER, f'{org_name}.json')
+        if os.path.exists(results_path):
+            lg.info(f'[{org_name}] Skipping - results already exist.')
+            continue
+
+        async with aiohttp.ClientSession() as session:
+            gh = GitHubAPI(session, 'web3-investigation', oauth_token=os.environ['GITHUB_TOKEN'])
+
+            lg.info(f'[{org_name}] Retrieving repos')
+            repos = []
+            async for repo in gh.getiter(f'/orgs/{org_name}/repos?per_page=100'):
+                repos.append(repo)
+            print(len(repos))
+
+            with open('tmp.json', 'w') as f:
+                json.dump(repos, f)
+
+            tasks = [process_repo(gh, org_name, repo) for repo in repos]
+            results = await asyncio.gather(*tasks)
+            results = [r for r in results if r is not None]
+
+        with open(results_path, 'w') as f:
+            json.dump({ 'org_name': org_name, 'results': results}, f)
+        lg.info(f'{len(results)} results found. Written to {results_path}')
+
+
+# Run the main function
+asyncio.run(main())
