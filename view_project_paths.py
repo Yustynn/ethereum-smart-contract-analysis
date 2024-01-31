c6bf2da9e07f10563fb218339eb710686ab765ed
Yustynn Panicker <yustynn@gmail.com>
2024-01-31 10:10:14 +0700

    [main] Generate results for all repos

---
+++ view_project_paths.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from config import project_configs
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+
+OUTPUT_PATH = 'output/paths.txt'
+from Project import Project
+
+logging.basicConfig(level=logging.INFO)
+lg = logging.getLogger('view_project_files')
+
+
+paths = []
+with logging_redirect_tqdm():
+  progress_bar = tqdm(project_configs)
+  for cfg in progress_bar:
+    progress_bar.set_description(f'Project: {cfg.name}')
+    project = Project(cfg)
+    paths += list({os.path.dirname(p) for p in project.get_path_candidates()})
+
+with open(OUTPUT_PATH, 'w') as f:
+  f.write('\n'.join(sorted(paths)))
+print(f'{len(paths)} saved to {OUTPUT_PATH}')
